10fd2609a21acb7da823ee833c35d27bf8ddefaf
Guido22 <GuidoDulos@valori.nl>
2022-02-03 13:35:34 +0100

    versie 1: method welcome en melding einde programma

--- src/main/java/FirstDemo.java
+++ src/main/java/FirstDemo.java
@@ -1,5 +1,11 @@
 public class FirstDemo {
 
     public static void main(String[] args) {
+        welcome();
+        System.out.println("Einde programma");
+    }
+
+    public static void welcome() {
+        System.out.println("Hallo trainees!");
     }
 }
