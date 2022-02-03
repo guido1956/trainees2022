da815963ed3a4128c363449bcdade0c5c3821cb6
Guido22 <GuidoDulos@valori.nl>
2022-02-03 14:25:51 +0100

    versie 2: demoConcatenatie

--- src/main/java/FirstDemo.java
+++ src/main/java/FirstDemo.java
@@ -2,10 +2,15 @@ public class FirstDemo {
 
     public static void main(String[] args) {
         welcome();
+        demoConcatenatie();
         System.out.println("Einde programma");
     }
 
     public static void welcome() {
         System.out.println("Hallo trainees!");
+    }
+
+    public static void demoConcatenatie() {
+        System.out.println("Dit is een tekst " + ", een lange tekst!");
     }
 }
