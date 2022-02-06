9aa364712e2330da106b0f460155422dc32c365b
Guido22 <GuidoDulos@valori.nl>
2022-02-06 10:16:17 +0100

    versie 14 demo JUnit

--- src/main/java/Arithmetic.java
+++ src/main/java/Arithmetic.java
@@ -1,16 +1,4 @@
 public class Arithmetic {
-    public static void main(String[] args) {
-        int number1 = 36;
-        int number2 = 5;
-        double number3 = 15;
-        double number4 = 2;
-        int result1 = 0;
-        double result2 = 0;
-        result1 = add(number1, number2);
-        result2 = add(number3, number4);
-        System.out.println(result1);
-        System.out.println(result2);
-    }
 
     public static int add(int a, int b) {
         return a + b;
